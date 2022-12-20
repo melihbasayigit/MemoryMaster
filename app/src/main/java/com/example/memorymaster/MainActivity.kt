8e405f0afc675bb0bf986ac9d2ac827bdb285ad7
Melih Basayigit <melihbasayigit@gmail.com>
2022-12-20 14:40:24 +0300

    cust addded trim func

--- app/src/main/java/com/example/memorymaster/MainActivity.kt
+++ app/src/main/java/com/example/memorymaster/MainActivity.kt
@@ -41,7 +41,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.textViewForgotPassword.setOnClickListener {
-            resetPassword(binding.editTextTextEmailAddress.text.toString())
+            resetPassword(binding.editTextTextEmailAddress.text.toString().trim { it <= ' ' })
         }
 
 
